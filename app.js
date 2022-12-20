84fb4e2465c83ffa9074d05c7fe15d15f9935419
Kalashkg153 <82664447+Kalashkg153@users.noreply.github.com>
2022-12-20 10:25:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 
 const express = require('express');
 const ejs = require('ejs');
-
+const path = require('path');
 const app = express();
 
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+
+app.set('views' , path.join(__dirname , 'views'));
+app.use(express.static(__dirname + '/public'));
+
+
 
 app.get("/" , function(req,res){
 
